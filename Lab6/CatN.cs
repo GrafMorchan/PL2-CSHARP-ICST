fbadf9b2a220a1ec7f3658811655c5554315fd46
GrafMorchan <114511145+GrafMorchan@users.noreply.github.com>
2025-02-28 08:54:37 +0500

    Add files via upload

---
+++ Lab6/CatN.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CatN : Cat
+{
+    private int k;
+
+    public int GetK() => k;
+
+    public CatN(string name) : base(name)
+    {
+        this.k = 0;
+    }
+
+    public override void Meow(int? n = null)
+    {
+        base.Meow(n);
+        if (n is null)
+            k++;
+        else if (n != 0)
+            k += (int)n;
+    }
+}
